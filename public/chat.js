7dc5f6442b9583c342ca44ae1b68a42287ec3ff1
kanikadawar5 <kanikadawar5@gmail.com>
2019-02-25 12:14:35 +0530

    first commit

---
+++ public/chat.js
@@ -0,0 +1,17 @@
+let socket = io.connect('localhost:8023');
+
+var person = prompt("Please enter your name:", "harshits..");
+//Handel Frontend request
+document.getElementById("send").addEventListener('click',function(){    
+    socket.emit('chat',{
+        message:message.value,
+        handle:person,
+        IP:"123"
+    });
+});
+
+socket.on('chat',function(data){
+    // document.getElementById("output").innerHTML += '<br><strong>'+data.handle+'</strong> : '+data.message+'    <strong> '+data.IP+'</strong>';
+    document.getElementById("output").innerHTML += '<br><strong>'+data.handle+'</strong> : '+data.message;
+
+});
